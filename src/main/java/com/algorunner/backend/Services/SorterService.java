cac18fd956e4e0ea5634aa8fd35918e67dbd379e
danielstayer2 <119252615+danielstayer2@users.noreply.github.com>
2024-09-13 20:00:53 +0000

    still need to make the return

--- src/main/java/com/algorunner/backend/Services/SorterService.java
+++ src/main/java/com/algorunner/backend/Services/SorterService.java
@@ -41,6 +41,18 @@ public class SorterService {
      * @return a SortedReturn object containing the sorted array and the time taken to sort
      */
     public SortedReturn selectionSort(int[] nums){
-        
+        int n = nums.length;
+
+        for (int i = 0; i < n-1; i++){
+            int minIdx = i;
+            for (int j = i+1; j < n; j++){
+                if (nums[j] < nums[minIdx]){
+                    minIdx = j;
+                }
+            }
+            int temp = nums[minIdx];
+            nums[minIdx] = nums[i];
+            nums[i] = temp;
+        }
     }
 }
