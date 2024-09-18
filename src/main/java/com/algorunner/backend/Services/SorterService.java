a19ec0935f34c7e15d08f033b8a2d8dba96810ee
danielstayer2 <119252615+danielstayer2@users.noreply.github.com>
2024-09-18 15:11:23 +0000

    added count sort

--- src/main/java/com/algorunner/backend/Services/SorterService.java
+++ src/main/java/com/algorunner/backend/Services/SorterService.java
@@ -63,7 +63,30 @@ public class SorterService {
     }
     //Quick Sort
     //Heap sort
-    //Counting Sort
+    public static SorterService countSort(int[] nums){
+        int N = inputArray.length;
+        int M = 0;
+
+        for (int i = 0; i < N; i++) {
+            M = Math.max(M, inputArray[i]);
+        }
+
+        int[] countArray = new int[M + 1];
+
+        for (int i = 0; i < N; i++) {
+            countArray[inputArray[i]]++;
+        }
+
+        for (int i = 1; i <= M; i++) {
+            countArray[i] += countArray[i - 1];
+        }
+
+        int[] outputArray = new int[N];
+
+        for (int i = N - 1; i >= 0; i--) {
+            outputArray[countArray[inputArray[i]] - 1] = inputArray[i];
+            countArray[inputArray[i]]--;
+    }
     //Radix Sort
     //Bucket Sort
     //...
